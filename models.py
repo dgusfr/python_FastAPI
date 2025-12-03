e53064a465e410fadb79b69877edd6b84a04284b
dgusfr <diegogustavo.fr@gmail.com>
2025-12-03 10:01:58 -0300

    feat(models): add class Matricula

--- models.py
+++ models.py
@@ -5,3 +5,9 @@ from database import Base
 class Estudante(Base):
     __tablename__ = "estudantes"
     id = Column(Integer, PramaryKey=True, index=True)
+    nome = Column(String(100), nullable=False)
+    age = Column(Integer)
+
+
+class Matricula(Base):
+    __tablename__ = "matriculas"
