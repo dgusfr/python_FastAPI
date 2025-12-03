4ea7079c78bc36a4b88362f403ee00e873bca4b7
dgusfr <diegogustavo.fr@gmail.com>
2025-12-03 10:18:08 -0300

    feat(schemas): add class estudante responde and Matricula base

--- schemas.py
+++ schemas.py
@@ -8,3 +8,16 @@ class EstudanteBase(BaseModel):
 
 class EstudanteCreate(EstudanteBase):
     pass
+
+
+# Definimos que podemos ler os campos do estudante direto do modelo
+class EstudanteResponse(EstudanteBase):
+    id: int
+
+    class Config:
+        from_atributes = True
+
+
+class MatriculaBase(BaseModel):
+    student_id: int
+    discipline_name: str
