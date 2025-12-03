a8a08ff0a8f2a05a857392ea65cf62806359763f
dgusfr <diegogustavo.fr@gmail.com>
2025-12-03 09:53:08 -0300

    feat(add): define models

--- models.py
+++ models.py
@@ -1,2 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from database import Base
+
+
+class Estudante(Base):
+    __tablename__ = "estudantes"
+    id = Column(Integer, PramaryKey=True, index=True)
